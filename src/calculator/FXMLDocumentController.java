882f79ab192c8979b8c0bb319dd25fdca7b2adc9
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2021-08-09 23:37:06 -0300

    Commit

--- src/calculator/FXMLDocumentController.java
+++ src/calculator/FXMLDocumentController.java
@@ -23,8 +23,8 @@ import javafx.scene.input.MouseEvent;
 public class FXMLDocumentController implements Initializable {
 
     @FXML
-    private Button closeButton; 
-  
+    private Button closeButton;
+    
     @FXML
     private TextField screen;
     
@@ -51,83 +51,134 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void number7(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 7;
+        } else {
+            num2 = 7;
+        }
     }
 
     @FXML
     private void number8(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 8;
+        } else {
+            num2 = 8;
+        }
     }
 
     @FXML
     private void number9(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 9;
+        } else {
+            num2 = 9;
+        }
     }
 
     @FXML
     private void number4(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 4;
+        } else {
+            num2 = 4;
+        }
     }
 
     @FXML
     private void number5(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 5;
+        } else {
+            num2 = 5;
+        }
     }
 
     @FXML
     private void number6(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 6;
+        } else {
+            num2 = 6;
+        }
     }
 
     @FXML
     private void number1(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 1;
+        } else {
+            num2 = 7;
+        }
     }
 
     @FXML
     private void number2(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 2;
+        } else {
+            num2 = 2;
+        }
     }
 
     @FXML
     private void number3(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 3;
+        } else {
+            num2 = 3;
+        }
     }
 
     @FXML
     private void number0(ActionEvent event) {
-        
+        if(!hasNum1) {
+            num1 = 0;
+        } else {
+            num2 = 0;
+        }
     }
 
-    @FXML
-    private void opEquals(ActionEvent event) {
-         
+    private void hasNum1() {
+        hasNum1 = num1 >= 0;
     }
 
     @FXML
     private void opSum(ActionEvent event) {
-        
+        if(hasNum1) {
+            op = 1;
+        }
     }
-    
+
     @FXML
     private void opLess(ActionEvent event) {
-        
+        if(hasNum1) {
+            op = 2;
+        }
     }
-    
+
     @FXML
     private void opDivision(ActionEvent event) {
-         
+        if(hasNum1) {
+            op = 3;
+        }
     }
-    
+
     @FXML
     private void opMultiply(ActionEvent event) {
-         
+        if(hasNum1) {
+            op = 4;
+        }
     }
-    
-    private void hasNum1() {
-        
+
+    @FXML
+    private void opEquals(ActionEvent event) {
+        if(num1>=0 && num2>=0) {
+
+        }
     }
-    
-   
-    
+
+
+
+
 }
